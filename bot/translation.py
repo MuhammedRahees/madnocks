9d629e1e338c2961d4f9c8b4fa4bd15f0b815fce
MuhammedRahees <88150389+MuhammedRahees@users.noreply.github.com>
2021-11-17 13:40:13 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey bro {}!!.....🙋</b>
+<i>Am <b>MOVIE BAAZAARS</b>Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+<i> Welcome to <b>M_D</b>🔥🔥😝
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+ More Details Click Help Button Below..
+
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@m0nuttan
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/joinchat/2kBSBV7gWbQwYzI1</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://git.heroku.com/madnocks.git">Click Me</a></i>
 """
